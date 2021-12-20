0409f7781a71a3f639be44cadf9703d04228d3d2
midurbar <midurbar@gmail.com>
2021-12-20 22:52:45 +0100

    Edita index.html, creado completed.html

--- index.php
+++ index.php
@@ -19,6 +19,24 @@
                 color:'blue',
                 shape:'pill',
                 label: 'pay'
+            },
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: 100
+                        }
+                    }]
+                })
+            },
+            onApprove: function(data, actions){
+                actions.order.capture().then(function(detalles){
+                    window.location.href="completed.html";
+                });
+            },
+            onCancel: function(data) {
+                alert("Pago cancelado");
+                console.log(data);
             }
         }).render('#paypal-button-container');
     </script>
