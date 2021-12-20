494865ed04487ea8258877b5784b045103fbcc14
midurbar <midurbar@gmail.com>
2021-12-20 20:36:16 +0100

    Creado projecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=Aetzy0y0068msubaINMRw9VIioFBM_qXehhY5dC8U1mGO_F9gy-jwDfHFS-vkwe2EMgh0VwjNjllKrxH&currency=EUR">
+
+    </script>
+</head>
+<body>
+
+    <div id="paypal-button-container"></div>
+    <script>
+        paypal.Buttons().render('#paypal-button-container');
+    </script>
+    
+</body>
+</html>
